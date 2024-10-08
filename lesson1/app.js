d39ac918b8f4b4eb5e33a42068cb4fa606c5d49c
Daniyal Ganyatov <deni12349075@gmail.com>
2024-10-08 22:11:51 +0300

    homework1

---
+++ lesson1/app.js
@@ -0,0 +1,19 @@
+/*
+Задание 1
+let degree = 20
+
+let fahrenheit = (9 / 5) * degree + 32
+
+console.log("Температура в цельсиях:", degree)
+
+console.log("Температура в фарингейтах:", fahrenheit)
+
+*/
+
+// Задание 2
+
+let names = "Your name"
+
+let admin = names
+
+console.log(admin)
