bde408ae0a765393dc4529965d62537ca721541d
Daniyal Ganyatov <deni12349075@gmail.com>
2024-10-08 22:41:18 +0300

    homework 4

---
+++ lesson4/app.js
@@ -0,0 +1,46 @@
+//Задание 1
+
+// let num1 = parseFloat(prompt("Enter your first number:"))
+// let num2 = parseFloat(prompt("Enter your second number:"))
+
+// if (num1 <= 1 && num2 >= 3){
+//     console.log("Conditions done")
+// }
+// else{
+//     console.log("Error 404")
+// }
+
+//Задание 2
+
+// let test = true
+
+// if (test === true) {
+//     console.log("correct")
+// }else {console.log("error 404")}
+
+// console.log(test ? 'correct' : 'false')
+
+//Задание 3
+
+// let day = parseFloat(prompt('Enter which day:'))
+
+// let decade
+// if (day >= 1 && day <= 10) {
+//     decade = 'first'
+// } else if (day > 10 && day <= 20) {
+//     decade = 'second'
+// } else if (day > 20 && day <= 31) {
+//     decade = 'third'
+// }else{
+//     decade= 'incorrect date'
+// }
+// console.log(`day ${day} coming to ${decade} decade of month`)
+
+//Задание 4
+
+// let number = parseFloat(prompt("Enter number:"))
+
+// let units = number % 10
+// let tens = Math.floor((number % 100) / 10)
+// let hundred = Math.floor(number / 100)
+// console.log(`Number of ${number} count to:${hundred}, tens of:${tens}, units:${units}`)
