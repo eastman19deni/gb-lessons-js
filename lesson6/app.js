8a5b16ed296d3946b9d49e2ffd73dfb7bb686f13
Daniyal Ganyatov <deni12349075@gmail.com>
2024-10-16 23:29:28 +0300

    lesson 6 homework

---
+++ lesson6/app.js
@@ -0,0 +1,71 @@
+//Задание 1
+// function cube(number){
+//     return number **3;
+// }
+
+// let result = cube(2) + cube(3);
+
+// console.log(result)
+//---------------------------------------------------------------------------
+//Задание 2
+// function taxSalary(salaryTax){
+//     return salaryTax * 0.87
+// }
+
+// let screen = prompt('Enter number: ')
+// let number = parseFloat(screen)
+
+// if(isNaN(number)){
+//     console.log('incorrect statement')
+// }else{
+//     let Tax = taxSalary(number)
+//     console.log(`${Tax},ваша заработочная плата после вычита налогов`)
+// }
+//-----------------------------------------------------------------------------
+//Задание 3
+
+// function maxNumber(a, b, c) {
+//     let max = a;
+//     if (b > max) {
+//         max = b;
+//     }
+//     if (c > max) {
+//         max = c;
+//     }
+//     return max;
+// }
+
+// let first = parseFloat(prompt('Enter first number:'))
+// let second = parseFloat(prompt('Enter second number:'))
+// let third = parseFloat(prompt('Enter third number:'))
+
+// let max = maxNumber(first, second, third);
+
+// console.log('The biggest number is:', max);
+//---------------------------------------------------------------------------
+
+//Задание 4
+
+// function addintion(a,b){
+//     return a+b;
+// }
+
+// function substraction(a,b){
+//     return a-b;
+// }
+
+// function multiply(a,b){
+//     return a*b;
+// }
+
+// function divide(a,b){
+//     return b !== 0 ? a/b :'error 404';
+// }
+
+// number1 = parseFloat(prompt('Enter first number:'));
+// number2 = parseFloat(prompt('Enter second number:'));
+
+// console.log(addintion(number1,number2));
+// console.log(substraction(number1,number2));
+// console.log(multiply(number1,number2));
+// console.log(divide(number1,number2));
