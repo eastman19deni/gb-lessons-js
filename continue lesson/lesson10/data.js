098d73c6d0118f81bd0ba3a7fee812d7c616f619
Daniyal Ganyatov <deni12349075@gmail.com>
2024-11-03 01:14:04 +0300

    created data file

---
+++ continue lesson/lesson10/data.js
@@ -0,0 +1,2 @@
+
+const dataJSON = '[{"id":1,"title":"Product 1","description":"Description for product 1","price":10.99,"image":"https://via.placeholder.com/150"},{"id":2,"title": "Product 2", "description": "Description for product 2","price":12.99,"image":"https://via.placeholder.com 150"},{"id":3, "title": "Product 3", "description": "Description for product 3","price":15.99,"image":"https://via.placeholder.com/150"}]';
